3af44527e5526beda48ffc960b99b33c91d534f2
gralucky254 <117747248+gralucky254@users.noreply.github.com>
2023-03-17 19:04:04 +0300

    Add files via upload

--- airbnb.js
+++ airbnb.js
@@ -1,5 +1,7 @@
 let subMenu = document.getElementById("subMenu");
+let showHome = document.getElementById("showHome");
 
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
+    showHome.classList.toggle("open-menu")
 }
